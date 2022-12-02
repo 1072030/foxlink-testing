f0f399291afb28a464614f5a104f480fa3861c45
B10615035 <B10615035@mail.ntust.edu.tw>
2022-12-03 01:38:05 +0800

    fix: runner.sh test7 breaktime

--- scripts/testings/runner.sh
+++ scripts/testings/runner.sh
@@ -10,17 +10,6 @@ then
     # reset mqtt server
     bash scripts/systems/clean_server.sh emqx
     bash scripts/systems/start_server.sh emqx
-
-elif [[ $1 == "remote" ]];
-then
-
-    # reset foxlink database
-    bash scripts/systems/clean_server.sh db
-    bash scripts/systems/start_server.sh db $SCENARIO_DB_TAG
-
-    # reset all remote servers (emqx, backend,api-db)
-    bash scripts/testings/restart_servers.sh
-
 else
     echo "please specify the valid condition..."
     exit 0
@@ -39,9 +28,12 @@ then
 
     # create time
     python -m app.utils.create_time -f $SCENARIO -s "$SHIFT_TIME_T2"
+elif [ $SCENARIO == "test7" ];
+then
+    python -m app.utils.create_time -f $SCENARIO -b 150
 else
     # create time
-    python -m app.utils.create_time -f $SCENARIO -b 150
+    python -m app.utils.create_time -f $SCENARIO 
 fi
 
 
