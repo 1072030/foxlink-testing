a3b79969a7bc736f8e838b6a90643ff5b14fb818
foxlink@ntust <>
2022-12-12 19:21:29 +0800

    sync db scripts from factory

--- scripts/db/export_db.sh
+++ scripts/db/export_db.sh
@@ -2,9 +2,9 @@ HOST=127.0.0.1
 PORT=27001
 USER=root
 DB_NAME=foxlink
-DB_BACKUP_NAME=test_api_backup_v3_shift5.sql
+PASSWD=AqqhQ993VNto
+DB_BACKUP_NAME=./bk.sql
 echo "Dump all DB..."
-
-mysqldump --lock-all-tables -h $HOST -u $USER -P $PORT -p $DB_NAME > $DB_BACKUP_NAME
+mysqldump --lock-all-tables -h $HOST -u $USER -P $PORT -p $DB_NAME --password=$PASSWD > $DB_BACKUP_NAME
 
 echo "Done !"
