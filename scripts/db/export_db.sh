3408e098493ffeb24c4b50ca50fa9c847adcbf8f
foxlink@ntust <>
2022-12-21 16:42:15 +0800

    update db scripts

--- scripts/db/export_db.sh
+++ scripts/db/export_db.sh
@@ -2,9 +2,9 @@ HOST=127.0.0.1
 PORT=27001
 USER=root
 DB_NAME=foxlink
-PASSWD=AqqhQ993VNto
+MYSQL_PWD=AqqhQ993VNto
 DB_BACKUP_NAME=./bk.sql
 echo "Dump all DB..."
-mysqldump --lock-all-tables -h $HOST -u $USER -P $PORT -p $DB_NAME --password=$PASSWD > $DB_BACKUP_NAME
+mysqldump --lock-all-tables -h $HOST -u $USER -P $PORT $DB_NAME -p$MYSQL_PWD > $DB_BACKUP_NAME
 
 echo "Done !"
--- scripts/db/import_db.sh
+++ scripts/db/import_db.sh
@@ -2,7 +2,7 @@ HOST=127.0.0.1
 PORT=27001
 USER=root
 DB_NAME=foxlink
-PASSWD=AqqhQ993VNto
+MYSQL_PWD=AqqhQ993VNto
 
 
 if [[ -z $1 ]];
@@ -12,6 +12,6 @@ then
 fi
 
 echo "Importing DB... $1"
-mysqladmin -h $HOST -P $PORT -u $USER --password=$PASSWD -p create $DB_NAME
-mysql -h $HOST -P $PORT -u $USER -p $DB_NAME --password=$PASSWD < $1
+mysqladmin -h $HOST -P $PORT -u $USER create -p$MYSQL_PWD $DB_NAME
+mysql -h $HOST -P $PORT -u $USER $DB_NAME -p$MYSQL_PWD < $1
 echo "Done !"
