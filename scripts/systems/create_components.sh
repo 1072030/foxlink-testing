15560851211dfabc15cbcbc6153f6f93daeacf0f
1072030 <229jdu@gmail.com>
2023-09-27 13:09:09 +0800

    update detail

--- scripts/systems/create_components.sh
+++ scripts/systems/create_components.sh
@@ -1,9 +1,9 @@
 . scripts/systems/envs.sh
 # create backend server image
-# cd $BACKEND_SERVER_DOCKER_CONTEXT
-# docker build -t incubator:init . -f $BACKEND_SERVER_DOCKER_FILE
+cd $BACKEND_SERVER_DOCKER_CONTEXT
+docker build -t incubator:init . -f $BACKEND_SERVER_DOCKER_FILE
 # create testing database image
 cd $TESTING_DB_DOCKER_CONTEXT
 docker build -t mysql-test:debian . -f $TESTING_DB_DOCKER_FILE
 # create required network
-# docker network  create --attachable incubator-network
+docker network  create --attachable incubator-network
